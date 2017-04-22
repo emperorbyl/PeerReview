863d8963a6f875a33c136c8cd7297befa9c88bfb
Justin Fairbourn <justin.fairbourn@gmail.com>
2017-04-21 19:12:22 -0600

    First Commit

---
+++ HW7/cmake-build-debug/CMakeFiles/HW7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW7.dir/main.cpp.obj"
+  "HW7.pdb"
+  "HW7.exe"
+  "HW7.exe.manifest"
+  "libHW7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
